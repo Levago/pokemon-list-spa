f444e6d9fa05be005b0e9ee03573353f925deb8b
Jorge Luis Marmolejo santos <jorgel.marmolejo96@hotmail.com>
2022-07-04 21:23:12 -0500

    create alumnoInfo.jsx file

---
+++ pokemon-list-spa/src/components/alumnInfo.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AlumnInfo = () => {
+    return (
+        <div>
+          <h1>Informacion del Alumno</h1>
+          <h3>Nombre:</h3>
+          <p>Jorge Luis Marmolejo Santos</p>
+          <h3>Matricula:</h3>
+          <p>170909</p>
+          <h3>Materia:</h3>
+          <p>Desarollo Wep Profesional</p>
+        </div>
+      );
+    
+}
+
+
+export default AlumnInfo;
--- pokemon-list-spa/src/index.js
+++ pokemon-list-spa/src/index.js
@@ -2,30 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AlumnInfo from './components/alumnInfo';
+import Pokemons from './components/pokemons';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 
 ReactDOM.render( <
-        BrowserRouter >
-        <
-        Routes >
-        <
-        Route path = '/'
-        element = { <
-            App / >
-        }
-        /><
-        Route path = '/pokemonInfo'
-        element = { < div > Pokemon info < /div>} / >
-            <
-            Route path = '/pokemonInfo'
-            element = { < div > Pokemon info < /div>} / > <
-                Route path = '/alumnInfo'
-                element = { < div > Informacion Del Alumno < /div>} / >
-                    <
-                    /
-                    Routes >
-                    <
-                    /BrowserRouter > ,
-                    document.getElementById('root')
-                );
+    BrowserRouter >
+    <
+    Routes >
+    <
+    Route path = '/pokemons'
+    element = { <
+        App / >
+    }
+    /><
+    Route path = '/pokemon'
+    element = { < Pokemons / > }
+    / > <
+    Route path = '/Matricula'
+    element = { < AlumnInfo / > }
+    / > <
+    Route path = '*'
+    element = { < Navigate replace to = '/pokemons' /
+        >
+    }
+    / > < /
+    Routes >
+    <
+    /BrowserRouter > ,
+    document.getElementById('root')
+);
