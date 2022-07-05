e02b568a4a6a3355100e8d2518db6bb878543444
Jorge Luis Marmolejo santos <jorgel.marmolejo96@hotmail.com>
2022-07-04 20:10:49 -0500

    create the routes on index.js

--- pokemon-list-spa/src/App.jsx
+++ pokemon-list-spa/src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
+import React from 'react'
 import PokemonThumb from './components/pokemons.jsx'
 
 const App = () => {
--- pokemon-list-spa/src/index.js
+++ pokemon-list-spa/src/index.js
@@ -2,12 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 ReactDOM.render( <
-    React.StrictMode >
-    <
-    App / >
-    <
-    /React.StrictMode>,
-    document.getElementById('root')
-);
+        BrowserRouter >
+        <
+        Routes >
+        <
+        Route path = '/'
+        element = { <
+            App / >
+        }
+        /><
+        Route path = '/pokemonInfo'
+        element = { < div > Pokemon info < /div>} / >
+            <
+            Route path = '/pokemonInfo'
+            element = { < div > Pokemon info < /div>} / > <
+                Route path = '/alumnInfo'
+                element = { < div > Informacion Del Alumno < /div>} / >
+                    <
+                    /
+                    Routes >
+                    <
+                    /BrowserRouter > ,
+                    document.getElementById('root')
+                );
